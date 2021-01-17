19b30bcb66ecc604d062a8a7c55f6f163eaab81a
Sophie Gu <sophieqgu@gmail.com>
2021-01-17 04:54:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Infinity)
+
+let mod = 4 % 9;
+
+let max = Math.max(0, 5, 7, 20);
